0e2270a17ff275c517ec33f0fdf9e792b0e0a3c4
Ryan Dias <ryan@flatcircle.io>
2018-04-23 13:40:31 +0200

    Branched off draw-gradient-line; merged master into draw-gradient-line-experimental

--- Source/Charts/Data/Implementations/Standard/PieChartData.swift
+++ Source/Charts/Data/Implementations/Standard/PieChartData.swift
@@ -23,6 +23,20 @@ open class PieChartData: ChartData
         super.init(dataSets: dataSets)
     }
 
+    /// - returns: All DataSet objects this ChartData object holds.
+    @objc open override var dataSets: [ChartDataSetProtocol]
+    {
+        get
+        {
+            assert(super.dataSets.count <= 1, "Found multiple data sets while pie chart only allows one")
+            return super.dataSets
+        }
+        set
+        {
+            super.dataSets = newValue
+        }
+    }
+
     @objc var dataSet: PieChartDataSetProtocol?
     {
         get
